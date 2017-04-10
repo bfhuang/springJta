d19683c85035beddbeeb87ab2091ddf87f506065
Binfang Huang <bfhuang@CNbfhuang.local>
2017-04-10 15:22:33 +0800

    fix the test

--- src/test/java/com/hello/HelloControllerTest.java
+++ src/test/java/com/hello/HelloControllerTest.java
@@ -27,6 +27,6 @@ public class HelloControllerTest {
     public void getHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("hello word")));
+                .andExpect(content().string(equalTo("hello world")));
     }
 }
